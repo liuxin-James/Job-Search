65057e155fe27fc682b7daee0f347e43724ed134
liuxin-James <xl9jv@virginia.edu>
2022-06-28 23:55:13 -0400

    Update bugs in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,57 +1,64 @@
-import React, {Fragment, useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import Spinner from '../layout/Spinner';
-import {getCurrentProfile, deleteAccount} from '../../actions/profile';
-import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
 import DashboardActions from './DashboardActions';
-import Education from './Education';
 import Experience from './Experience';
+import Education from './Education';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
-const Dashboard = ({getCurrentProfile, auth: {user}, deleteAccount, profile:{profile, loading}}) => {
-    useEffect(() => {
-        getCurrentProfile();
-    }, [getCurrentProfile]);
-    return loading && profile === null ? <Spinner /> : 
+const Dashboard = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile }
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  return (
     <section className="container">
-        <Fragment>
-            <h1 className='large text-primary'>Dashboard</h1>
-            <p className='lead' >
-            <i className='fas fa-user'> Welcome {user && user.name}</i></p>
-        </Fragment>
-        {profile !== null ? (
-            <Fragment>
-                <DashboardActions />
-                <Experience experience = {profile.experience}/>
-                <Education education = {profile.education}/>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <>
+          <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
 
-                <div className="my-2">
-                    <button className="btn btn-danger" onClick={() => deleteAccount()}>
-                        <i className="fas fa-user-minus" /> Delete My Account
-                    </button>
-                </div>
-            </Fragment>
-        ):(
-            <Fragment>
-                <p>You have not setup your profile, please add now</p>
-                <Link to='/create-profile' className='btn btn-primary my-1'>
-                    Create Profile
-                </Link>
-            </Fragment>
-        )}
+          <div className="my-2">
+            <button className="btn btn-danger" onClick={() => deleteAccount()}>
+              <i className="fas fa-user-minus" /> Delete My Account
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </>
+      )}
     </section>
+  );
 };
 
 Dashboard.propTypes = {
-    getCurrentProfile: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    deleteAccount: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
-    auth: state.auth,
-    profile: state.profile
+  auth: state.auth,
+  profile: state.profile
 });
 
-export default connect(mapStateToProps , {getCurrentProfile, deleteAccount})(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
