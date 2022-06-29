c734715f72997ebeef8e1be9981e3249b78758a5
liuxin-James <xl9jv@virginia.edu>
2022-06-29 01:01:12 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
 import { LOGOUT } from './actions/types';
+import NotFound from './components/layout/NotFound';
 
 //Reudx
 import {Provider} from 'react-redux';
@@ -65,6 +66,7 @@ const App = () => {
             <Route path="add-education" element={<PrivateRoute component={AddEducation} />} />
             <Route path="posts" element={<PrivateRoute component={Posts} />} />
             <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
+            <Route path="/*" element={<NotFound />} />
           </Routes>
         </Fragment>
     </Router>
